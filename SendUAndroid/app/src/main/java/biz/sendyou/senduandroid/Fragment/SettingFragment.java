6d70a8fab6adebb8ceaf4e0677b427245467a48a
하준혁 <enoxaiming@naver.com>
2016-08-17 11:30:46 +0900

    Remove SettingPreferenceFragment

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SettingFragment.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SettingFragment.java
@@ -1,6 +1,8 @@
 package biz.sendyou.senduandroid.Fragment;
 
-import android.app.Fragment;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -12,7 +14,7 @@ import biz.sendyou.senduandroid.R;
 /**
  * Created by Chan_Woo_Kim on 2016-08-11.
  */
-public class SettingFragment extends Fragment{
+/*public class SettingFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +25,63 @@ public class SettingFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(R.id.content, new SettingPreferenceFragment()).commit();
+
+    }
+
+    public static SettingFragment newInstance() {
+        SettingFragment fragment = new SettingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private class SettingPreferenceFragments extends PreferenceFragment {
+        public SettingPreferenceFragments() {}
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.xml_preference);
+        }
+    }
+}*/
+
+public class SettingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View mView = inflater.inflate(R.layout.fragment_setting, container,false);
+        return mView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static SettingFragment newInstance() {
+        SettingFragment fragment = new SettingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        public SettingsFragment() {}
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.xml_preference);
+        }
     }
 }
+
+
