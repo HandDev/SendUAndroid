66df65b7067c7839073c4ba0eb2287a198f3bbf2
박재성 <jspark141515@gmail.com>
2016-08-17 20:55:09 +0900

    Created Fragments for making card

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SelectTemplateFragment.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SelectTemplateFragment.java
@@ -1,7 +1,88 @@
 package biz.sendyou.senduandroid.Fragment;
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import biz.sendyou.senduandroid.R;
+
 /**
- * Created by pyh42 on 2016-08-17.
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SelectTemplateFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SelectTemplateFragment#newInstance} factory method to
+ * create an instance of this fragment.
  */
-public class SelectTemplateFragment {
+public class SelectTemplateFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public SelectTemplateFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static SelectTemplateFragment newInstance() {
+        SelectTemplateFragment fragment = new SelectTemplateFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_select_template2, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
 }
