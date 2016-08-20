1da7c97dad4017b53b78a30e2ef9e144867ee66a
박재성 <jspark141515@gmail.com>
2016-08-20 12:10:07 +0900

    Created SelectTemplateFragment as containing RecyclerView + TemplateRecyclerViewAdapter
    Created CardTemplate class for saving objects of card templates. Saves drawable object

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SelectTemplateFragment.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SelectTemplateFragment.java
@@ -1,36 +1,58 @@
 package biz.sendyou.senduandroid.Fragment;
 
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import biz.sendyou.senduandroid.R;
+import biz.sendyou.senduandroid.datatype.CardTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SelectTemplateFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SelectTemplateFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A fragment representing a list of Items.
+ * <p>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
  */
 public class SelectTemplateFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private static final String ARG_TEMPLATE = "card-templates";
+    private final String LOGTAG = "SelectTemplateFragment";
+    // TODO: Customize parameters
+    private int mColumnCount = 2;
+    private OnListFragmentInteractionListener mListener;
 
+    //TODO: DummyDatas should not be saved as static
+    private static List<CardTemplate> dummyTemplates;
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    //Create Dummy data for testing recyclerViewAdapter
     public SelectTemplateFragment() {
-        // Required empty public constructor
+
     }
 
-
-    // TODO: Rename and change types and number of parameters
-    public static SelectTemplateFragment newInstance() {
+    @SuppressWarnings("unused")
+    public static SelectTemplateFragment newInstance(List<CardTemplate>inputTemplates ,int columnCount) {
         SelectTemplateFragment fragment = new SelectTemplateFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
 
+        //TODO Remove dummyData setting code
+        dummyTemplates = inputTemplates;
         return fragment;
     }
 
@@ -38,30 +60,39 @@ public class SelectTemplateFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_template, container, false);
+        View view = inflater.inflate(R.layout.fragment_template_list, container, false);
+
+        // Set the adapter
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.templatelist);
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new TemplateRecyclerViewAdapter(dummyTemplates, mListener));
+            Log.i(LOGTAG, "setRecyclerViewAdapter");
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnListFragmentInteractionListener");
         }
     }
 
@@ -76,13 +107,13 @@ public class SelectTemplateFragment extends Fragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p/>
+     * <p>
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onListFragmentInteraction(CardTemplate item);
     }
 }
