ee01906668893995d2a281bb9548fbd5879a1f9b
하준혁 <enoxaiming@naver.com>
2016-08-23 23:01:19 +0900

    Add SignUpProcessActivity

---
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SignUpProcessActivity.java
@@ -0,0 +1,18 @@
+package biz.sendyou.senduandroid.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import biz.sendyou.senduandroid.R;
+
+/**
+ * Created by JunHyeok on 2016. 8. 23..
+ */
+public class SignUpProcessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_record);
+    }
+}
