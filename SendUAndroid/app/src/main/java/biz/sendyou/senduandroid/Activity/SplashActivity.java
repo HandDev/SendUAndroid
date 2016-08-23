3096e2f3b729d43c2c77607c8666458109208546
박재성 <jspark141515@gmail.com>
2016-08-23 22:49:30 +0900

    Created checking is first launch after installation in SplashActivity

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SplashActivity.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SplashActivity.java
@@ -1,19 +1,42 @@
 package biz.sendyou.senduandroid.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import biz.sendyou.senduandroid.R;
+import biz.sendyou.senduandroid.thread.TemplateDownloadThread;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final String LOGTAG = "SplashActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        boolean isFirstStart = false;
+        SharedPreferences pref;
+        pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+
+        if(!pref.getBoolean("isFirst", false)){
+            Log.i(LOGTAG,"isFirst launch after installation");
+            //start Template Image Download Thread
+            TemplateDownloadThread templateDownloadThread = new TemplateDownloadThread();
+            templateDownloadThread.execute();
+            Log.i(LOGTAG, "Thread execute");
+
+            SharedPreferences.Editor edit = pref.edit();
+            edit.putBoolean("workCheckBox", true);
+            edit.putBoolean("isFirst", true);
+            edit.commit();
+
+        }
         //Very normal Splash!!
         Handler mHandler = new Handler() {
             @Override
@@ -27,5 +50,22 @@ public class SplashActivity extends AppCompatActivity {
         };
 
         mHandler.sendEmptyMessageDelayed(0,3000); // Delay 3 sec.
+
     }
+
+    private void callOnBoardingActivity(){
+        //Very normal Splash!!
+        Handler mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                Intent mIntent = new Intent(SplashActivity.this, OnBoardingActivity.class);
+                startActivity(mIntent);
+                finish();
+            }
+        };
+
+    }
+
 }
---
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/thread/TemplateDownloadThread.java
@@ -0,0 +1,7 @@
+package biz.sendyou.senduandroid.thread;
+
+/**
+ * Created by parkjaesung on 2016. 8. 23..
+ */
+public class TemplateDownloadThread {
+}
