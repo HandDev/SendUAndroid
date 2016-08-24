081b77b40403bb3de3d2172e57396f8ade03809c
하준혁 <enoxaiming@naver.com>
2016-08-24 21:42:25 +0900

    Add gmail auth function ( need to add json file )

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SignUpProcessActivity.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SignUpProcessActivity.java
@@ -13,6 +13,6 @@ public class SignUpProcessActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup_record);
+        setContentView(R.layout.activity_signup_input);
     }
 }
--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SignupActivity.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SignupActivity.java
@@ -1,9 +1,14 @@
 package biz.sendyou.senduandroid.Activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,17 +20,26 @@ import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import biz.sendyou.senduandroid.R;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private CallbackManager mCallBackManager;
     private LoginButton mFacebookSignInButton;
     private GoogleApiClient mGoogleApiClient;
     private SignInButton mGoogleSignInButton;
+    private int mSignInRequestCode = 0;
+    public static Context signupContext;
+    public static SignupActivity mSignupActivity;
+    private String LOGTAG = "SignUp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +52,11 @@ public class SignupActivity extends AppCompatActivity {
         ActionBar mActionBar = getSupportActionBar();
         mActionBar.hide();
 
-        TextView mTextView01 = (TextView)findViewById(R.id.sendu);
-        TextView mTextView02 = (TextView)findViewById(R.id.Select);
+        TextView mTextView01 = (TextView) findViewById(R.id.sendu);
+        TextView mTextView02 = (TextView) findViewById(R.id.Select);
 
-        TypefaceHelper.getInstance().setTypeface(mTextView01,"NotoSansCJKkr-Regular.otf");
-        TypefaceHelper.getInstance().setTypeface(mTextView02,"NotoSansCJKkr-Regular.otf");
+        TypefaceHelper.getInstance().setTypeface(mTextView01, "NotoSansCJKkr-Regular.otf");
+        TypefaceHelper.getInstance().setTypeface(mTextView02, "NotoSansCJKkr-Regular.otf");
 
         mFacebookSignInButton = (LoginButton) findViewById(R.id.fb_btn);
         mGoogleSignInButton = (SignInButton) findViewById(R.id.gmail_btn);
@@ -54,12 +68,13 @@ public class SignupActivity extends AppCompatActivity {
         mFacebookSignInButton.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
         mFacebookSignInButton.setText("");
         mGoogleSignInButton.setBackgroundResource(R.drawable.sg_gmail);
+        //mGoogleSignInButton.setVisibility(View.INVISIBLE);
 
         mFacebookSignInButton.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 final Profile mProfile = Profile.getCurrentProfile();
-                Snackbar.make(getCurrentFocus(), mProfile.getName(), Snackbar.LENGTH_SHORT).show();
+                callSignUpProcessActivity();
             }
 
             @Override
@@ -74,9 +89,51 @@ public class SignupActivity extends AppCompatActivity {
         });
 
 
+        mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mGoogleApiClient == null) {
+                    GoogleSignInOptions mGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestEmail()
+                            .build();
 
+                    mGoogleApiClient = new GoogleApiClient.Builder(SignupActivity.this)
+                            .addApi(Auth.GOOGLE_SIGN_IN_API, mGoogleSignInOptions)
+                            .build();
+                }
+                googleSignIn();
+            }
+        });
+    }
 
+    private void googleSignIn() {
+        Intent mSignIn = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(mSignIn, mSignInRequestCode);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == mSignInRequestCode) {
+            Log.e(LOGTAG,String.valueOf(requestCode));
+            Log.e(LOGTAG,String.valueOf(mSignInRequestCode));
+            GoogleSignInResult mGoogleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Log.e(LOGTAG,Boolean.toString(mGoogleSignInResult.isSuccess()));
+            if (mGoogleSignInResult.isSuccess()) {
+                GoogleSignInAccount mGoogleSignInAccount = mGoogleSignInResult.getSignInAccount();
+                callSignUpProcessActivity();
+            }
+        }
+    }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public static void callSignUpProcessActivity() {
+        Intent mIntent = new Intent(SignupActivity.mSignupActivity, SignUpProcessActivity.class);
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        signupContext.startActivity(mIntent);
     }
 }
