962a023af5149923734070cf85e39564371517be
박재성 <jspark141515@gmail.com>
2016-08-27 19:39:16 +0900

    Renamed layout xml file fragment_dialog -> fragment_create_card_textdialog

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/InputDialogFragment.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/InputDialogFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +24,7 @@ public class InputDialogFragment extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_fragment, container);
+        View view = inflater.inflate(R.layout.fragment_create_card_textdialog, container);
         return view;
     }
 
