bbec3ea47ce68021f2ee5665cef2763438d2e59f
HaJunHyeok <pawn@boxbreakers.co>
2016-09-25 14:04:53 +0900

    Email Check Function with Retrofit

---
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Service/EmailCheck.java
@@ -0,0 +1,15 @@
+package biz.sendyou.senduandroid.Service;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by JunHyeok on 2016. 9. 23..
+ */
+
+public interface EmailCheck {
+    @GET("email/{Email}")
+    Call<Repo> emailck(@Path("Email") String email);
+}
+
