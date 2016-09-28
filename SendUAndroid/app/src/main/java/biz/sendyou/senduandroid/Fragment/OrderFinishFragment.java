17aa43fb8eed2f4f1890f026340b4eb2d80ebd88
HaJunHyeok <pawn@boxbreakers.co>
2016-09-28 11:42:13 +0900

    OrderCheck Fragment function add

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/OrderFinishFragment.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/OrderFinishFragment.java
@@ -38,16 +38,12 @@ public class OrderFinishFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment OrderFinishFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static OrderFinishFragment newInstance(String param1, String param2) {
+    public static OrderFinishFragment newInstance() {
         OrderFinishFragment fragment = new OrderFinishFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SendCheckFragment.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/SendCheckFragment.java
@@ -3,12 +3,17 @@ package biz.sendyou.senduandroid.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +22,7 @@ import biz.sendyou.senduandroid.Activity.LoginActivity;
 import biz.sendyou.senduandroid.Fragment.item.SendCheckItem;
 import biz.sendyou.senduandroid.R;
 import biz.sendyou.senduandroid.Service.OrderList;
+import biz.sendyou.senduandroid.Service.Orders;
 import biz.sendyou.senduandroid.Service.Repo;
 import biz.sendyou.senduandroid.Service.doOrder;
 import retrofit2.Call;
@@ -32,33 +38,40 @@ public class SendCheckFragment extends Fragment {
 
     private static final String URL = "http://52.78.159.163:8080/";
 
+    private List<SendCheckItem> mList;
+
     public static SendCheckFragment newInstance(){
         return new SendCheckFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
         View mView = inflater.inflate(R.layout.fragment_send_check_list, container, false);
 
+        ImageView arrow = (ImageView)mView.findViewById(R.id.arrow);
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FrontFragment frontFragment = FrontFragment.newInstance();
+                ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+                getFragmentManager().beginTransaction().replace(R.id.mainFrameLayout, frontFragment).commit();
+            }
+        });
         RecyclerView mRecyclerView = (RecyclerView) mView.findViewById(R.id.sendcheck_recyclerview);
         LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(mView.getContext());
         mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
 
-
+        mList = new ArrayList();
         getOrderList();
-
-        List<SendCheckItem> mList = new ArrayList();
-        mList.add(new SendCheckItem("(15255) 경기도 안산시 단원구 사세충열로 94 한국디지털미디어고등학교", "김수아", "2016.07.25", "2016.07.26", R.drawable.doing));
-        mList.add(new SendCheckItem("(15255) 경기도 안산시 단원구 사세충열로 94 한국디지털미디어고등학교", "김지우", "2016.07.25", "2016.07.26", R.drawable.todo));
-        mList.add(new SendCheckItem("(15255) 경기도 안산시 단원구 사세충열로 94 한국디지털미디어고등학교", "강진아", "2016.07.25", "2016.07.26", R.drawable.done));
-        mList.add(new SendCheckItem("(15255) 경기도 안산시 단원구 사세충열로 94 한국디지털미디어고등학교", "김수아", "2016.07.25", "2016.07.26", R.drawable.doing));
-        mList.add(new SendCheckItem("(15255) 경기도 안산시 단원구 사세충열로 94 한국디지털미디어고등학교", "김지우", "2016.07.25", "2016.07.26", R.drawable.todo));
-        mList.add(new SendCheckItem("(15255) 경기도 안산시 단원구 사세충열로 94 한국디지털미디어고등학교", "강진아", "2016.07.25", "2016.07.26", R.drawable.done));
-
+        Log.e("size",String.valueOf(mList.size()));
         mRecyclerView.setAdapter(new SendCheckRecyclerViewAdapter(mList, mView.getContext()));
         return mView;
     }
+
 
     @Override
     public void onPause() {
@@ -76,19 +89,38 @@ public class SendCheckFragment extends Fragment {
 
         OrderList orderList = retrofit.create(biz.sendyou.senduandroid.Service.OrderList.class);
 
-        Call<Repo> call = orderList.getOrderList(LoginActivity.email);
+        Call<ArrayList<Orders>> call = orderList.getOrderList(LoginActivity.email);
         Log.e("email",LoginActivity.email);
 
-        call.enqueue(new Callback<Repo>() {
+        call.enqueue(new Callback<ArrayList<Orders>>() {
             @Override
-            public void onResponse(Call<Repo> call, Response<Repo> response) {
+            public void onResponse(Call<ArrayList<Orders>> call, Response<ArrayList<Orders>> response) {
+                ArrayList<Orders> arrayList = response.body();
                 Log.e("Log",String.valueOf(response.isSuccessful()));
                 Log.e("Log",response.message());
                 Log.e("Log",response.raw().toString());
+                for(int i = 0; i < response.body().size(); i++) {
+                    int image;
+                    if(arrayList.get(i).getOrderStatus().equals("Ordered")) {
+                        image = R.drawable.doing;
+                    }
+                    else if(arrayList.get(i).getOrderStatus().equals("Ordered")) {
+                        image = R.drawable.todo;
+                    }
+                    else if(arrayList.get(i).getOrderStatus().equals("Ordered")) {
+                        image = R.drawable.done;
+                    }
+                    else {
+                        image = R.drawable.sample_image;
+                    }
+                    mList.add(new SendCheckItem("("+arrayList.get(i).getNumAddress()+") "+arrayList.get(i).getAddress(),arrayList.get(i).getReceiverName(),arrayList.get(i).getOrderDate(),arrayList.get(i).getOrderDate(),image));
+                }
+
+
             }
 
             @Override
-            public void onFailure(Call<Repo> call, Throwable t) {
+            public void onFailure(Call<ArrayList<Orders>> call, Throwable t) {
 
             }
         });
