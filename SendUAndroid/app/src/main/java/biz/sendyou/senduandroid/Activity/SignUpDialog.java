4dec3db65acc26a8b9944b26995eb8ea9deaac6b
HaJunHyeok <pawn@boxbreakers.co>
2016-09-29 22:50:02 +0900

    Dialog Function Finish

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SignUpDialog.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SignUpDialog.java
@@ -5,22 +5,38 @@ import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import biz.sendyou.senduandroid.ContextManager;
 import biz.sendyou.senduandroid.R;
+import biz.sendyou.senduandroid.Service.EmailCheck;
+import biz.sendyou.senduandroid.Service.Repo;
+import biz.sendyou.senduandroid.URLManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by JunHyeok on 2016. 9. 29..
  */
 
 public class SignUpDialog extends AppCompatActivity {
+
+    private EditText mNameEditText,mEmailEditText,mPasswordEditText,mPasswordCheckEditText;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,7 +51,11 @@ public class SignUpDialog extends AppCompatActivity {
         alertBuilder.show();
 
         ImageView close = (ImageView)layout.findViewById(R.id.close_btn);
-
+        mNameEditText = (EditText)layout.findViewById(R.id.signup_name_edittext);
+        mEmailEditText = (EditText) layout.findViewById(R.id.signup_email_edittext);
+        mPasswordEditText = (EditText) layout.findViewById(R.id.signup_password_edittext);
+        mPasswordCheckEditText = (EditText) layout.findViewById(R.id.signup_password_check_edittext);
+        Button mSignupButton = (Button) layout.findViewById(R.id.signup_next_btn);
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,7 +63,66 @@ public class SignUpDialog extends AppCompatActivity {
             }
         });
 
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mNameEditText.getText().toString().matches("") || mEmailEditText.getText().toString().matches("") || mPasswordCheckEditText.getText().toString().matches("") || mPasswordEditText.getText().toString().matches("")) {
+                    Toast.makeText(getBaseContext(), "빈칸이 있는지 확인해주세요.", Toast.LENGTH_LONG).show();
+                } else if (!isValidEmail(mEmailEditText.getText().toString())) {
+                    Toast.makeText(getBaseContext(), "이메일의 형식이 아닙니다. 다시 한번 확인해주세요.", Toast.LENGTH_LONG).show();
+                } else if (mPasswordEditText.getText().length() < 8) {
+                    Toast.makeText(getBaseContext(), "비밀번호가 너무 짧습니다. 8자 이상으로 작성해주세요.", Toast.LENGTH_LONG).show();
+                } else if (!mPasswordEditText.getText().toString().matches(mPasswordCheckEditText.getText().toString())) {
+                    Toast.makeText(getBaseContext(), "비밀번호가 일치하지 않습니다. 다시 확인해주세요.", Toast.LENGTH_LONG).show();
+                } else {
+                    emailck();
+                }
+            }
+
+        });
+
 
     }
+
+    private static boolean isValidEmail(String s) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches();
+    }
+
+    private void emailck() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URLManager.authURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EmailCheck emailCheck = retrofit.create(EmailCheck.class);
+
+        Call<Repo> call = emailCheck.emailck(mEmailEditText.getText().toString());
+
+        call.enqueue(new Callback<Repo>() {
+            @Override
+            public void onResponse(Call<Repo> call, Response<Repo> response) {
+                Repo repo = response.body();
+
+                if(repo.isSuccess()){
+                    /*Intent mIntent = new Intent(signupInputActivity, SignupAddressActivity.class);
+                    mIntent.putExtra("userName", mFirstNameEditText.getText().toString() + mNameEditText.getText().toString());
+                    mIntent.putExtra("email", mEmailEditText.getText().toString());
+                    mIntent.putExtra("password", mPasswordCheckEditText.getText().toString());
+                    mIntent.putExtra("birth",mBirthEditText.getText().toString());
+                    startActivity(mIntent);*/
+                }
+
+                else {
+                    //Toast.makeText(signupInputActivity, "이미 가입된 이메일입니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Repo> call, Throwable t) {
+
+            }
+        });
+    }
+
 
 }
