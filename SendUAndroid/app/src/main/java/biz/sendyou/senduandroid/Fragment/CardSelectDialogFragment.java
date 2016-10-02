5cdc85f43a3d40b094add0cf807aa2b0b6e64518
ParkJaesung <jspark141515@gmail.com>
2016-10-02 20:10:41 +0900

    Created CardSelectDialogFragment and layout

---
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Fragment/CardSelectDialogFragment.java
@@ -0,0 +1,103 @@
+package biz.sendyou.senduandroid.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import biz.sendyou.senduandroid.R;
+import biz.sendyou.senduandroid.URLManager;
+
+public class CardSelectDialogFragment extends DialogFragment {
+
+    private final String LOGTAG = "CardDialogFragment";
+
+    private String imageURL; //cardID is used for s3 connection
+    private ImageLoader imageLoader = ImageLoader.getInstance();
+
+    private OnFragmentInteractionListener mListener;
+
+    public CardSelectDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static CardSelectDialogFragment newInstance(String imageURL) {
+
+        CardSelectDialogFragment fragment = new CardSelectDialogFragment();
+        fragment.setImageURL(imageURL);
+        return fragment;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_card_select_dialog, container, false);
+
+        final ImageView preview = (ImageView)view.findViewById(R.id.templatepreview);
+        imageLoader.displayImage(URLManager.s3URL + imageURL, preview);
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
