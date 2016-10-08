5329963536c489a2fe9587165ba29273ad7cd02a
dudgns0507 <hoon2585@gmail.com>
2016-10-08 20:14:52 +0900

    Add fade_out animation

--- SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SplashActivity.java
+++ SendUAndroid/app/src/main/java/biz/sendyou/senduandroid/Activity/SplashActivity.java
@@ -15,6 +15,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import biz.sendyou.senduandroid.ActivityManager;
@@ -118,6 +120,7 @@ public class SplashActivity extends AppCompatActivity {
     public void intentActivty(Context packageContext, Class cls) {
         Intent mIntent = new Intent(packageContext, cls);
         startActivity(mIntent);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         finish();
     }
 
